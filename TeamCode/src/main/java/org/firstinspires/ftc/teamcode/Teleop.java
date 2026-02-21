1c459a846cd6ed2d45871158dbc42fa63deb1fbc
Andychoudhury <134175183+Andychoudhury@users.noreply.github.com>
2026-02-20 20:22:12 -0600

    tweaked front autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -66,7 +66,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Teleop", group="Linear OpMode")
+@TeleOp(name="Teleop", group="aLinear OpMode")
 
 public class Teleop extends LinearOpMode {
 
